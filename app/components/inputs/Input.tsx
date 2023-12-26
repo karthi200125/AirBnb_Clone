b95111f9678e0c798680656eb0dd6c1be177e83c
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-12-26 20:56:13 +0530

    Update Input.tsx

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -6,7 +6,7 @@ import { BiDollar } from "react-icons/bi";
 interface InputProps {
     id: string;
     label: string;
-    type: string;
+    type?: string;
     disabled: boolean;
     formatPrice: boolean;
     required: boolean;
