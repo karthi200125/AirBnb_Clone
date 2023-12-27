b45c0d371af044c614f9b9d02f3fe89fa42621d2
karthick <skarthikeyan25052001@gmail.com>
2023-12-27 12:46:35 +0530

    neeew

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
-import getCurrentUser from "../../../actions/getCurrentUser";
 
 interface IParams {
   listingId?: string;
