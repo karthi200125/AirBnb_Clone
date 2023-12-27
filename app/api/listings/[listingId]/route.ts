afde908be64cdb6e6b5e42eb43c829a252517135
karthick <skarthikeyan25052001@gmail.com>
2023-12-27 12:33:40 +0530

    change current user import

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
-import getCurrentUser from "@/app/actions/getCurrentUser";
+import getCurrentUser from "../../../actions/getCurrentUser";
 
 interface IParams {
   listingId?: string;
